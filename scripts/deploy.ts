0b0c1c44546e1d1c315583ab5d9a669e30b673c1
Jeremiah Samuel <livinalt@gmail.com>
2024-02-27 21:51:23 +0100

    updating deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,17 +6,17 @@ async function main() {
   // await budyToken.waitForDeployment();
 
   
-  const budyPlatform = await ethers.deployContract("BudyPlatform");
-  await budyPlatform.waitForDeployment();
+  // const budyPlatform = await ethers.deployContract("BudyPlatform");
+  // await budyPlatform.waitForDeployment();
  
-  // const BudyPlatformFactory = await ethers.deployContract("BudyPlatformFactory");
-  // await BudyPlatformFactory.waitForDeployment();
+  const BudyPlatformFactory = await ethers.deployContract("BudyPlatformFactory");
+  await BudyPlatformFactory.waitForDeployment();
 
 
 
-  // console.log(`BudyToken has been deployed to ${budyToken.target}`); // 0x72Bf75788F41175d6741DF740CA67Ea67cB13c27
-  console.log(`BudyPlatform has been deployed to ${budyPlatform.target}`);
-  // console.log(`BudyPlatformFactory has been deployed to ${BudyPlatformFactory.target}`); // 0x7d97b692c67f9A484C7b6Ded046AD7D7D486eBD8 // 0x007209d5254d48793161e0ab5568ecd1dad001160f0706e710d8da38e466561a
+  // console.log(`BudyToken has been deployed to ${budyToken.target}`); // 0x38C09eF13F988BE3dB073CFD8e4ed874eeDD5acD
+  // console.log(`BudyPlatform has been deployed to ${budyPlatform.target}`);
+  console.log(`BudyPlatformFactory has been deployed to ${BudyPlatformFactory.target}`); // 0xD426581713Dd89ac11aC65Fa53ffAc89D7e024bC // 0x007209d5254d48793161e0ab5568ecd1dad001160f0706e710d8da38e466561a
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
